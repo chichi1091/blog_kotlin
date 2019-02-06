e397fd76d4d331b23934ab264472c8e480692a21
Chiharu Terashima <chichi.pa2.1091@gmail.com>
2019-02-06 21:21:33 +0900

    入力チェック追加

--- src/main/kotlin/com/example/blog/web/RegisterController.kt
+++ src/main/kotlin/com/example/blog/web/RegisterController.kt
@@ -1,6 +1,8 @@
 package com.example.blog.web
 
 import com.example.blog.model.RegisterModel
+import com.example.blog.repository.UserRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
@@ -10,15 +12,24 @@ import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
-class RegisterController{
+class RegisterController(
+        private val userRepository: UserRepository
+        , private val passwordEncoder: PasswordEncoder
+) {
     @GetMapping("/register")
     fun index(model: Model): String {
+        model.addAttribute("appName", "Kotlin Blog")
         model.addAttribute("form", RegisterModel())
         return "register"
     }
 
     @PostMapping("/register")
     fun register(@ModelAttribute("form") @Validated form: RegisterModel, result: BindingResult, model: Model): String {
+        if (result.hasErrors()) {
+            model.addAttribute("appName", "Kotlin Blog")
+            return "register"
+        }
+
         return "redirect:/home/"
     }
 }
