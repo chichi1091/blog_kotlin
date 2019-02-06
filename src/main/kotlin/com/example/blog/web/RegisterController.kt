2e9308b1dbae8d0cb1025ef80c22d1305498cf1a
Chiharu Terashima <chichi.pa2.1091@gmail.com>
2019-02-06 21:13:59 +0900

    登録ボタンメソッド

--- src/main/kotlin/com/example/blog/web/RegisterController.kt
+++ src/main/kotlin/com/example/blog/web/RegisterController.kt
@@ -3,7 +3,11 @@ package com.example.blog.web
 import com.example.blog.model.RegisterModel
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class RegisterController{
@@ -11,5 +15,10 @@ class RegisterController{
     fun index(model: Model): String {
         model.addAttribute("form", RegisterModel())
         return "register"
+    }
+
+    @PostMapping("/register")
+    fun register(@ModelAttribute("form") @Validated form: RegisterModel, result: BindingResult, model: Model): String {
+        return "redirect:/home/"
     }
 }
